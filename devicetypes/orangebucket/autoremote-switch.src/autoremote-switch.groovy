ee0a095a4f3c0177db3243e4af60c0972592dc2d
Graham Johnson <grunson@gmail.com>
2018-05-23 13:49:24 +0100

    AutoRemote Switch version 1.1.

--- devicetypes/orangebucket/autoremote-switch.src/autoremote-switch.groovy
+++ devicetypes/orangebucket/autoremote-switch.src/autoremote-switch.groovy
@@ -1,18 +1,33 @@
 /*
+ * ---------------------------------------------------------------------------------
+ * (C) Graham Johnson (orangebucket)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER 
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ * ---------------------------------------------------------------------------------
+ *
  * AutoRemote Switch
  * =================
  * Virtual switch device handle that sends AutoRemote messages for 'on' and 'off' states.
  *
- * Author:				Graham Johnson
- * GitHub:				OrangeBucket
- * Namespace:			orangebucket
+ * Author:				Graham Johnson (orangebucket)
  *
- * Version:				1.0 (22/05/2018)
+ * Version:				1.1 (23/05/2018)
  *
  * Future plans:		Add notifications.	
  *
- * Changes:				1.0 (23/05/2018)		Initial release.
-  */
+ * Changes:				1.1 (23/05/2018)		Cosmetic changes to source code.
+ *						1.0 (23/05/2018)		Initial release.
+ */
 
 preferences
 {
