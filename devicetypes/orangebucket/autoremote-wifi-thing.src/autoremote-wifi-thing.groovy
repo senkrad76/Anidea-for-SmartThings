b44651941a0a3256a2912e662afd312922489a3c
Graham Johnson <grunson@gmail.com>
2018-10-02 09:50:36 +0100

    Version 1.5 tidies up the UI.

--- devicetypes/orangebucket/autoremote-wifi-thing.src/autoremote-wifi-thing.groovy
+++ devicetypes/orangebucket/autoremote-wifi-thing.src/autoremote-wifi-thing.groovy
@@ -3,7 +3,7 @@
  * (C) Graham Johnson (orangebucket)
  *
  * Permission to use, copy, modify, and/or distribute this software for any purpose
- * with or without fee is hereby granted, provided that the above copyright notice
+ * with or without fee is hereby granted, provided that the copyright notice below
  * and this permission notice appear in all copies.
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
@@ -17,25 +17,42 @@
  *
  * AutoRemote WiFi Thing
  * =====================
- * A SmartThings Device Handler for various 'things' that connects to devices
- * running the AutoRemote WiFi Service on the local LAN. Devices supported are:
+ * A SmartThings Device Handler that connects to devices running the AutoRemote WiFi
+ * Service on the local LAN and issues commands in the AutoApps command format. The
+ * commands are just URLs so the remote devices can do what they like in response.
  *
+ * The capabilities supported are:
+ *
+ *		Actuator (*)
  *		Alarm
  *		Notification
+ * 		Sensor (*)
+ *		Speech Synthesis
  *		Switch
  *		Tone
  *
+ * (*) The Actuator and Sensor capabilities are shown as deprecated in a Capabilities
+ * reference, yet elsewhere their use is encouraged as best practice. The Actuator capability
+ * just means the device has commands. The Sensor capability means it has attributes.
+ *
  * Author:				Graham Johnson (orangebucket)
  *
- * Version:				1.3		(01/10/2018)
+ * Version:				1.5		(02/10/2018)
  *
  * Comments:			Need to look at what parameters from the AutoRemote
  *						Send Message Service also apply to WiFi.
- *						The state change event is only triggered when a response to the 
+ *						A state change event is only triggered when a response to the 
  *                      request has been received. This doesn't mean it has 'worked',
- *						only that the remote device has received the request.
+ *						only that the remote device has received the request. However it
+ *						is a lot better than simply sending state change events in the
+ *						commands.
+ commands.
  *
- * Changes:				1.3		(01/10/2018)	Drop on/off message configuration and use
+ * Changes:				1.5		(02/10/2018)	Continuing tidy up as part of learning curve.
+ *												Add more intermediate states.
+ *						1.4		(01/10/2018)	Add Speech Synthesis. General tidy up and
+ *												improvement of code.
+ *						1.3		(01/10/2018)	Drop on/off message configuration and use
  *												AutoApps command format. Add Alarm, Tone and
  *												Notification capabilities.
  *						1.2		(05/06/2018)	State change events handled in parse method.
@@ -48,16 +65,15 @@
 
 preferences
 {
-		// input "message_on",     "text", 	title: "Message for 'On'", 						required: true,
-        // description: "AutoRemote WiFi message"
-		// input "message_off",    "text", 	title: "Message for 'Off'", 					required: true,
-        // description: "AutoRemote WiFi message"
-        // input "target",     	"text", 	title: "AutoRemote Target (Optional)",			required: false
-        // input "sender",     	"text", 	title: "AutoRemote Sender (Optional)",			required: false
-		// input "password",     	"password", title: "AutoRemote Password (Optional)",		required: false
-		// input "ttl",     		"text", 	title: "AutoRemote Validity time (Optional)",	required: false
-		// input "collapsekey",    "text", 	title: "AutoRemote Message Group (Optional)",	required: false
-        // input "showdebug",		"bool",		title: "Debug Messages",						required: true
+	// These preferences are used by the cloud based AutoRemote messaging. The password
+    // might be useful if it is honoured, the others probably aren't.
+    
+    // input "target", "text", title: "AutoRemote Target (Optional)", required: false
+	// input "sender", "text", title: "AutoRemote Sender (Optional)", required: false
+ 	// input "password", "password", title: "AutoRemote Password (Optional)", required: false
+	// input "ttl", "text", title: "AutoRemote Validity time (Optional)", required: false
+	// input "collapsekey", "text", title: "AutoRemote Message Group (Optional)", required: false
+    // input "showdebug", "bool", title: "Debug Messages", required: true
 }
 
 metadata
@@ -65,9 +81,11 @@ metadata
 	definition (name: "AutoRemote WiFi Thing", namespace: "orangebucket", author: "Graham Johnson")
     {
 		capability "Actuator"
-		capability "Switch"
         capability "Alarm"
         capability "Notification"
+        capability "Sensor"
+        capability "Speech Synthesis"
+		capability "Switch"
         capability "Tone"
 	}
     
@@ -75,56 +93,111 @@ metadata
     command "alarmoff"
     command "switchoff"
 
-	// Simulator.
+	// One day I will investigate this.
 	simulator
     {
 	}
 
 	// UI.
+    //
+    // White (#ffffff) is the standard for 'off'-like states.
+    // Blue (#00a0dc) is the standard for 'on'-like states.
+    // Orange (#e86d13) is the standard for devices requiring attention.
+    // Grey (#cccccc) is the standard for inactive or offline devices.
+    //
+    // Orange is used for the alarm 'Siren', 'Strobe' and 'Both' states as that is what is meant by
+    // a device requiring attention.
+    //
+    // The standards suggest that transitional states should use the colour for the transitioned to state.
+    // This seems such a baffling choice that it has been ignored. There also doesn't seem to be any conventions
+    // for tiles used for test and reset purposes.
+    //
+    // Yellow (#c0c000) is being used for transitional states to highlight possible technical issues.
+    // Purple (#800080) is being used for test tiles for notification, speech and tone.
+    // Red (#ff0000) is being used by tiles that reset the alarm and switch to off.
+    //
 	tiles
     {
-        standardTile("alarm", 			"device.alarm", width: 1, height: 1) 
+    	// Start with a tile for the alarm status of the device. Either turns both alarms on, or turns both off.
+        // Transitional states are shown while waiting for a response from the remote device.
+        standardTile("alarm", "device.alarm", width: 2, height: 2, canChangeIcon: true) 
         {
-            state "off", label:'Off', action:'alarm.siren', icon:"st.alarm.alarm.alarm", backgroundColor:"#ffffff"
-            state "siren", label: 'Siren', action:'alarmoff', icon:"st.alarm.alarm.alarm", backgroundColor:"#00a0dc"
+            state "off", label:'Off', action:'alarm.both', icon:"st.alarm.alarm.alarm", backgroundColor:"#ffffff", nextState: "bothon"         
+            state "bothon", label: '-> Both', icon: "st.alarm.alarm.alarm", backgroundColor: "#c0c000"
+            state "both", label: 'Both', action:'alarmoff', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13", nextState: "bothoff"
+            state "siren", label: 'Siren', action:'alarmoff', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13", nextState: "bothoff"
+            state "strobe", label: 'Strobe', action:'alarmoff', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13", nextState: "bothoff"              
+            state "bothoff", label: '-> Off', icon: "st.alarm.alarm.alarm", backgroundColor: "#c0c000"
+        }
+         
+        // This tile either adds the siren alarm, or turns both alarms off.
+        // Transitional states are shown while waiting for a response from the remote device.
+        standardTile("siren", "device.alarm", width: 1, height: 1) 
+        {
+            state "off", label:'Off', action:'alarm.siren', icon:"st.Electronics.electronics13", backgroundColor:"#ffffff", nextState: "sirenon"
+          	state "strobe", label:'Off', action:'alarm.both', icon:"st.Electronics.electronics13", backgroundColor:"#ffffff", nextState: "bothon"
+            state "sirenon", label: '-> Siren', icon:"st.Electronics.electronics13", backgroundColor:"#c0c000"
+            state "bothon", label: '-> Both', icon:"st.Electronics.electronics13", backgroundColor:"#c0c000"
+            state "siren", label: 'Siren', action:'alarmoff', icon:"st.Electronics.electronics13", backgroundColor:"#e86d13", nextState: "bothoff"
+            state "both", label: 'Both', action:'alarmoff', icon:"st.Electronics.electronics13", backgroundColor:"#e86d13", nextState: "bothoff"
+            state "bothoff", label: '-> Off', icon:"st.Electronics.electronics13", backgroundColor:"#c0c000"
+      	}        
+        
+        // This tile either adds the strobe alarm, or turns both alarms off.  
+        // Transitional states are shown while waiting for a response from the remote device.
+        standardTile("strobe", "device.alarm", width: 1, height: 1) 
+        {
+            state "off", label:'Off', action:'alarm.strobe', icon:"st.Lighting.light13", backgroundColor:"#ffffff", nextState: "strobeon"
+            state "siren", label:'Siren', action:'alarm.both', icon:"st.Lighting.light13", backgroundColor:"#ffffff", nextState: "bothon"
+            state "strobeon", label: '-> Siren', icon:"st.Lighting.light13", backgroundColor:"#c0c000"
+            state "bothon", label: '-> Both', icon:"st.Lighting.light13", backgroundColor:"#c0c000"
+            state "strobe", label: 'Strobe', action:'alarmoff', icon:"st.Lighting.light13", backgroundColor:"#e86d13", nextState: "bothoff"
+            state "both", label: 'Both', action:'alarmoff', icon:"st.Lighting.light13", backgroundColor:"#e86d13", nextState: "bothoff"
+            state "bothoff", label: '-> Off', icon:"st.Lighting.light13", backgroundColor:"#c0c000"
         }
 
-        standardTile("notification",	"device.notification", width: 1, height: 1)
+        // This tile will reset the alarm to off regardless of current status.
+		standardTile("alarmreset", "device.alarm", width: 1, height: 1)
         {
-            state "default", label:'Notify', action:"notification.deviceNotification", icon:"st.Kids.kids1"
-        }
-        
-		// Switch showing intermediate states while awaiting device response.
-		standardTile("switch",   		"device.switch", width: 1, height: 1, canChangeIcon: true)
-        {
-			state "off",     label: "Off", action: "switch.on",  icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turnon"
-			state "on",      label: "On",  action: "switchoff", icon: "st.switches.switch.on",  backgroundColor: "#00a0dc", nextState: "turnoff"
-            
-			state "turnoff", label: "Turning Off", icon: "st.switches.switch.off", backgroundColor: "#ff8000"
-            state "turnon",  label: "Turning On",  icon: "st.switches.switch.on",  backgroundColor: "#ff8000"
-        }
-        
-        // Try to force switch on.
-		standardTile("swon",  			"device.switch", width: 1, height: 1)
-        {
-			state "off", label: 'On',  action: "switch.on",  icon: "st.switches.switch.on",  backgroundColor: "#008000",
-            defaultState: true
+			state "reset", label: 'Reset', action: "alarmoff", icon: "st.alarm.alarm.alarm", backgroundColor: "#ff0000", defaultState: true
 		}
         
-        // Try to force switch off.
-		standardTile("swoff",			"device.switch", width: 1, height: 1)
+        // This tile sends a sample notification.
+        standardTile("notification", "device.notification", width: 1, height: 1)
         {
-			state "on", label: 'Off', action: "switchoff", icon: "st.switches.switch.off", backgroundColor: "#ff0000",
-            defaultState: true
-		}
-        
-        standardTile("tone", 			"device.tone", width: 1, height: 1)
-        {
-            state "default", label:'Tone', action:"tone.beep", icon:"st.alarm.beep.beep"
+            state "notify", label:'Notify', action:"notification.deviceNotification", icon:"st.Office.office13", backgroundColor:"#800080", defaultState: true
         }
         
-		main "alarm"
-        details (["alarm", "notification", "switch", "swon", "swoff", "tone"])
+        // This tile sends a sample text to be spoken.
+        standardTile("speak", "device.speech", width: 1, height: 1)
+        {
+            state "speak", label:'Speak', action:'Speech Synthesis.speak', icon:"st.Entertainment.entertainment3", backgroundColor:"#800080", defaultState: true
+        }
+        
+		// Switch showing transitional states while awaiting device response.
+		standardTile("switch", "device.switch", width: 1, height: 1)
+        {
+			state "off", label: "Off", action: "switch.on",  icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turnon"        
+            state "turnon", label: "-> On", icon: "st.switches.switch.on", backgroundColor: "#c0c000"
+			state "on", label: "On", action: "switchoff", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState: "turnoff"            
+			state "turnoff", label: "-> Off", icon: "st.switches.switch.off", backgroundColor: "#c0c000"
+        }
+                
+        // This tile will reset the switch to off regardless of current status.
+		standardTile("switchreset", "device.switch", width: 1, height: 1)
+        {
+			state "reset", label: 'Reset', action: "switchoff", icon: "st.switches.switch.off", backgroundColor: "#ff0000", defaultState: true
+		}
+        
+        standardTile("tone", "device.tone", width: 1, height: 1)
+        {
+            state "tone", label:'Tone', action:"tone.beep", icon:"st.alarm.beep.beep", backgroundColor: "#800080", defaultState: true
+        }
+        
+        // Use the alarm as the main tile.
+        main "alarm"
+        // Sort the tiles suitably.
+        details (["alarm", "siren", "strobe", "notification", "speak", "tone", "switch", "alarmreset", "switchreset"])
 	}
 }
 
@@ -159,11 +232,16 @@ def buildhubaction(thing, thingcommand, commandstate)
     // programmatically so it might as well be set once via the IDE rather than via
     // parameters.
 	def hex = device.getDeviceNetworkId()
+    
+    // URL encoding is probably a bit redundant and AutoRemote doesn't seem to do any
+    // decoding so it would break things if the whole query string was encoded. So
+    // just encode the variable that might have relatively free text in it.
+    def encthingcommand = URLEncoder.encode(thingcommand, 'UTF-8');
 	
 	def hubaction = new physicalgraph.device.HubAction(
         method	: "GET",
         path	: "/sendmessage",
- 		query	:	[ "message": "autoremotewifithing=:=${thing}=:=${thingcommand}" ],          	
+ 		query	:	[ "message": "autoremotewifithing=:=${thing}=:=${encthingcommand}" ],          	
         headers	:
             [
             	"HOST": "${hex}",
@@ -172,13 +250,23 @@ def buildhubaction(thing, thingcommand, commandstate)
     
     // Save any state change associated with this request.
     if (commandstate) state[hubaction.requestId] = "${thing}=:=${thingcommand}"
-    
-    return hubaction
+
+	return hubaction
+}
+
+def both()
+{
+	return buildhubaction('alarm', 'both', true)
 }
 
 def siren()
 {
 	return buildhubaction('alarm', 'siren', true)
+}
+
+def strobe()
+{
+	return buildhubaction('alarm', 'strobe', true)
 }
 
 // Custom command to turn alarm off.
@@ -193,7 +281,15 @@ def deviceNotification(notificationtext)
     if (!notificationtext?.trim()) notificationtext = "AutoRemote WiFi Thing"
    
 	// ST will run the HubAction for us.
-    return buildhubaction('notification', URLEncoder.encode(notificationtext, 'UTF-8'), false)
+    return buildhubaction('notification', notificationtext, false)
+}
+
+def speak(words)
+{
+    if (!words?.trim()) words = "AutoRemote WiFi Thing"
+   
+	// ST will run the HubAction for us.
+    return buildhubaction('speak', words, false)
 }
 
 def on()
@@ -217,7 +313,7 @@ def off()
 }
 
 // Custom command to turn switch off.
-def swoff()
+def switchoff()
 {
     // ST will run the HubAction for us.
     return buildhubaction('switch', 'off', true)
