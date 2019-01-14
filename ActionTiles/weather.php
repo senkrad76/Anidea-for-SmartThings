a376bb483d02c4bc60d40da5724e3561f0d52a09
orangebucket <39342399+orangebucket@users.noreply.github.com>
2019-01-14 10:02:30 +0000

    Update weather.php
    Updated for weather.php v1.0.0.

--- ActionTiles/weather.php
+++ ActionTiles/weather.php
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
 <?php
 //
-// (C) Graham Johnson 2018
+// weather.php (C) Graham Johnson 2018
+// ===================================
+// Version: 1.0.0   14/01/2019
 //
-// weather.php
-// ===========
-// Present three three-hourly weather forecasts in a bespoke ActionTiles compatible
-// form, sourcing the data from Met Office DataPoint.
+// ---------------------------------------------------------------------------------
+// Permission to use, copy, modify, and/or distribute this software for any purpose
+// with or without fee is hereby granted, provided that the copyright notice below
+// and this permission notice appear in all copies.
 //
-// Add the regional text forecasts from the same source.
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+// FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+// OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER 
+// TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+// THIS SOFTWARE.
+// ---------------------------------------------------------------------------------
+//
+// Present weather in a bespoke ActionTiles compatible form, sourcing the current 
+// observations from OpenWeatherMap and the three-hourly weather forecasts and the
+// regional text forecasts from the Met Office DataPoint API
 //
     
 $refresh  = isset( $_GET["refresh"]   ) ? $_GET["refresh"]  : '1800';                                 // Default refresh of 30 minutes.
+
+// OWM.
+$owmid    = isset( $_GET["owmid"]    )  ? $_GET["owmid"]    : '2651817';                              // Default is Croydon.
+$owmkey   = isset( $_GET["owmkey"]    ) ? $_GET["owmkey"]   : '<OWM API ID>';                         // OWM API ID.
+
+// Met Office DataPoint.
 $cityid   = isset( $_GET["cityid"]    ) ? $_GET["cityid"]   : '324152';                               // Default is Croydon.
 $regionid = isset( $_GET["regionid"]  ) ? $_GET["regionid"] : '514';                                  // Default is SE England.
-$apikey   = isset( $_GET["apikey"]    ) ? $_GET["apikey"]   : '<APIKEY>'; // DataPoint API Key.
+$apikey   = isset( $_GET["apikey"]    ) ? $_GET["apikey"]   : '<DATAPOINT API KEY'>;                  // DataPoint API Key.
 
 $showjson  = isset( $_GET["showjson"] ) ? $_GET["showjson"] : false;
 ?>
@@ -34,11 +53,16 @@ if ( $refresh )
             body                        { margin: 0; padding: 0; color: white; background: black; font-family: sans-serif;}
             .wrapper                    { margin: 4px; }
             .tiles                      { margin: 0 0 4px 0; padding: 0; overflow: auto; }
+            .owmtile                    { float: left; width: 100px; height: 100px; margin: 0 4px 0 0; padding: 0; 
+                                          color: #00c0c0; background: #000000; border: 1px solid #00c0c0; text-align: center; }
             .tile                       { float: left; width: 102px; height: 102px; margin: 0 4px 0 0; padding: 0; 
                                           color: #000000; background: #00c0c0; text-align: center; }
             .emptytile                  { float: left; width: 100px; height: 100px; margin: 0 4px 0 0; padding: 0; 
                                           color: #00c0c0; background: #000000; border: 1px solid #00c0c0; text-align: center; }
             .tiles > div:last-child     { margin-right: 0; }
+            .owmtemp                    { font-weight: bold; font-size: 48px; margin-top: 20px; }
+            .owmweather                 { font-weight: bold; font-size: 16px; }
+            .owmdescription             { font-size: 12px; }
             .fcrain                     { font-size: 16px; }
             .fctemp                     { font-weight: bold; font-size: 48px; }
             .fcfeels                    { font-size: 16px; }
@@ -56,10 +80,39 @@ if ( $refresh )
                 document.getElementById('d0').style.display = (tile == 'd0') ? 'block' : 'none';
                 document.getElementById('d1').style.display = (tile == 'd1') ? 'block' : 'none';
                 document.getElementById('d2').style.display = (tile == 'd2') ? 'block' : 'none';
+                document.getElementById('dptiles').scrollIntoView();
+                window.scrollBy(0, -4);
             }
         </script>
     </head>
     <body>
+        <div class="wrapper">
+<?php
+$owm = "http://api.openweathermap.org/data/2.5/weather?id=$owmid&appid=$owmkey";
+
+$ch = curl_init( $owm );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_POST,           0 );
+
+$resp =  curl_exec( $ch );
+curl_close( $ch );
+
+$json = json_decode( $resp, true );
+
+if ( $showjson ) echo "\n<pre>\n" . print_r( $json, true ) . "\n</pre>\n";
+
+?>
+            <div class="tiles">
+                <div class="owmtile">
+                     <div class="owmtemp"><?php echo round( $json[ 'main' ][ 'temp' ] - 273.15 ); ?>&deg;</div>
+                </div>
+                <div class="owmtile">
+                    <div class="owmweather"><?php echo $json[ 'weather' ][ 0 ][ 'main' ]; ?></div>
+                    <div class="fctemp"><img src="http://openweathermap.org/img/w/<?php echo $json[ 'weather' ][ 0 ][ 'icon' ]; ?>.png"></div>
+                    <div class="owmdescription"><?php echo ucwords( $json[ 'weather' ][ 0 ][ 'description' ] ); ?></div>
+                </div>
+                <div class="emptytile"></div>
+           </div>
 <?php
 $weathertype = array(   'Clear night', 
                         'Sunny day',
@@ -92,7 +145,7 @@ $weathertype = array(   'Clear night',
                         'Thunder shower (night)',
                         'Thunder shower (day)',
                         'Thunder' );
-    
+                        
 $met = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/$cityid?res=3hourly&key=$apikey";
 
 $ch = curl_init( $met );
@@ -106,8 +159,7 @@ $json = json_decode( $resp, true );
 
 if ( $showjson ) echo "\n<pre>\n" . print_r( $json, true ) . "\n</pre>\n";
 ?>
-       <div class="wrapper">
-            <div class="tiles">
+            <div class="tiles" id="dptiles">
 <?php
 $count = 0;
     
@@ -190,7 +242,7 @@ $periodcount = 0;
 foreach ( $json[ 'RegionalFcst' ][ 'FcstPeriods' ][ 'Period' ] as $period )	
 {
     // Check if there are multiple paragraphs in an array, or just the one.
-    if ( is_array( $period[ 'Paragraph' ][ 0 ] ) ) foreach ( $period[ 'Paragraph' ] as $paragraph )
+    if ( array_key_exists( 0, $period[ 'Paragraph' ] ) ) foreach ( $period[ 'Paragraph' ] as $paragraph )
     {
 ?>
                 <p><strong><?php echo $paragraph[ 'title' ]; ?></strong> <?php echo $paragraph[ '$' ]; ?></p>
