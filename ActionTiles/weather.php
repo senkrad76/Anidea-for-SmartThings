b78e750c9b0cf356a5bbf62f6b8db430d61ea4af
orangebucket <39342399+orangebucket@users.noreply.github.com>
2019-01-17 15:29:34 +0000

    Update weather.php
    Version 1.1.0 adds more forecast tiles and tweaks the look of the forecast tiles a bit.

--- ActionTiles/weather.php
+++ ActionTiles/weather.php
@@ -3,7 +3,7 @@
 //
 // weather.php (C) Graham Johnson 2018
 // ===================================
-// Version: 1.0.0   14/01/2019
+// Version: 1.1.0   17/01/2019
 //
 // ---------------------------------------------------------------------------------
 // Permission to use, copy, modify, and/or distribute this software for any purpose
@@ -28,12 +28,12 @@ $refresh  = isset( $_GET["refresh"]   ) ? $_GET["refresh"]  : '1800';           
 
 // OWM.
 $owmid    = isset( $_GET["owmid"]    )  ? $_GET["owmid"]    : '2651817';                              // Default is Croydon.
-$owmkey   = isset( $_GET["owmkey"]    ) ? $_GET["owmkey"]   : '<OWM API ID>';                         // OWM API ID.
+$owmkey   = isset( $_GET["owmkey"]    ) ? $_GET["owmkey"]   : '1bc13b73c241f4d71b6b72dcb944ae67';     // OWM API ID.
 
 // Met Office DataPoint.
 $cityid   = isset( $_GET["cityid"]    ) ? $_GET["cityid"]   : '324152';                               // Default is Croydon.
 $regionid = isset( $_GET["regionid"]  ) ? $_GET["regionid"] : '514';                                  // Default is SE England.
-$apikey   = isset( $_GET["apikey"]    ) ? $_GET["apikey"]   : '<DATAPOINT API KEY'>;                  // DataPoint API Key.
+$apikey   = isset( $_GET["apikey"]    ) ? $_GET["apikey"]   : 'e6bf3e19-bfca-4a07-a2ae-a263a725b76b'; // DataPoint API Key.
 
 $showjson  = isset( $_GET["showjson"] ) ? $_GET["showjson"] : false;
 ?>
@@ -49,38 +49,43 @@ if ( $refresh )
 }
 ?>
         <title>Weather Forecast</title>
-        <style type="text/css">
+        <style>
             body                        { margin: 0; padding: 0; color: white; background: black; font-family: sans-serif;}
             .wrapper                    { margin: 4px; }
-            .tiles                      { margin: 0 0 4px 0; padding: 0; overflow: auto; }
-            .owmtile                    { float: left; width: 100px; height: 100px; margin: 0 4px 0 0; padding: 0; 
+            .tiles                      { margin: 0 0 4px 0; padding: 0; overflow: auto; white-space: nowrap; font-size: 0;}
+            .owmtile                    { float: left; width: 100px; height: 100px; margin: 0 4px 0 0; padding: 0;
                                           color: #00c0c0; background: #000000; border: 1px solid #00c0c0; text-align: center; }
-            .tile                       { float: left; width: 102px; height: 102px; margin: 0 4px 0 0; padding: 0; 
+            .tile                       { display: inline-block; width: 102px; height: 102px; margin: 0 4px 0 0; padding: 0; 
                                           color: #000000; background: #00c0c0; text-align: center; }
-            .emptytile                  { float: left; width: 100px; height: 100px; margin: 0 4px 0 0; padding: 0; 
+            .emptytile                  { display: inline-block; width: 100px; height: 100px; margin: 0 4px 0 0; padding: 0; 
                                           color: #00c0c0; background: #000000; border: 1px solid #00c0c0; text-align: center; }
             .tiles > div:last-child     { margin-right: 0; }
-            .owmtemp                    { font-weight: bold; font-size: 48px; margin-top: 20px; }
+            .owmtime                    { font-size: 16px; }
+            .owmtemp                    { font-weight: bold; font-size: 48px; }
             .owmweather                 { font-weight: bold; font-size: 16px; }
             .owmdescription             { font-size: 12px; }
-            .fcrain                     { font-size: 16px; }
+            .owmwind                    { font-size: 16px; }
+            .owmspeed                   { font-weight: bold; font-size: 48px; }
+            .owmunit                    { font-size: 16px; }
+            .fctime                     { font-weight: bold; font-size: 16px; }
             .fctemp                     { font-weight: bold; font-size: 48px; }
-            .fcfeels                    { font-size: 16px; }
-            .description                { width: 308px; height: 96px; margin: 0 0 0 0; padding: 2px; 
+            .fcrain                     { font-weight: bold; font-size: 16px; }
+            .description                { width: 308px; height: 96px; margin: 0 0 0 0; padding: 2px; display: none;
                                           color: #00c0c0; background: #000000; border: 1px solid #00c0c0; text-align: center; }
             .weathertype                { font-size: 24px; }
-            #d0                         { display: block; text-align: left;   }
-            #d1                         { display: none;  text-align: center; }
-            #d2                         { display: none;  text-align: right;  }
             #textforecast               { color: #00c0c0; font-size: 14px; }
         </style>
         <script>
-            function displaydesc(tile)
+            function displaydesc( tilenum )
             {
-                document.getElementById('d0').style.display = (tile == 'd0') ? 'block' : 'none';
-                document.getElementById('d1').style.display = (tile == 'd1') ? 'block' : 'none';
-                document.getElementById('d2').style.display = (tile == 'd2') ? 'block' : 'none';
-                document.getElementById('dptiles').scrollIntoView();
+                var i;
+                
+                for ( i = 0; i < 9; i++ )
+                {
+                    document.getElementById( 't' + i ).style.background = (tilenum == i ) ? '0#008080' : '0#00c0c0';
+                    document.getElementById( 'd' + i ).style.display = (tilenum == i ) ? 'block' : 'none';
+                }
+                document.getElementById( 'fctiles' ).scrollIntoView();
                 window.scrollBy(0, -4);
             }
         </script>
@@ -101,18 +106,32 @@ $json = json_decode( $resp, true );
 
 if ( $showjson ) echo "\n<pre>\n" . print_r( $json, true ) . "\n</pre>\n";
 
+$cotime =  new DateTime( '@' . $json[ 'dt' ] );
+
+function compasspoint( $bearing ) 
+{ 
+    $points = array( 'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N');
+    
+    return $points[ round( $bearing / 22.5 ) ];
+}
+
 ?>
             <div class="tiles">
                 <div class="owmtile">
+                     <div class="owmtime"><?php echo $cotime->format( 'H:i' ); ?></div>
                      <div class="owmtemp"><?php echo round( $json[ 'main' ][ 'temp' ] - 273.15 ); ?>&deg;</div>
                 </div>
                 <div class="owmtile">
                     <div class="owmweather"><?php echo $json[ 'weather' ][ 0 ][ 'main' ]; ?></div>
-                    <div class="fctemp"><img src="http://openweathermap.org/img/w/<?php echo $json[ 'weather' ][ 0 ][ 'icon' ]; ?>.png"></div>
+                    <div><img src="https://openweathermap.org/img/w/<?php echo $json[ 'weather' ][ 0 ][ 'icon' ]; ?>.png"></div>
                     <div class="owmdescription"><?php echo ucwords( $json[ 'weather' ][ 0 ][ 'description' ] ); ?></div>
                 </div>
-                <div class="emptytile"></div>
-           </div>
+                <div class="owmtile">
+                    <div class="owmwind"><?php echo compasspoint( $json[ 'wind' ][ 'deg' ] ); ?></div>
+                    <div class="owmspeed"><?php echo round( $json[ 'wind' ][ 'speed' ]* 2.237 ); ?></div>
+                    <div class="owmunit">mph</div>
+                </div>
+            </div>
 <?php
 $weathertype = array(   'Clear night', 
                         'Sunny day',
@@ -159,7 +178,7 @@ $json = json_decode( $resp, true );
 
 if ( $showjson ) echo "\n<pre>\n" . print_r( $json, true ) . "\n</pre>\n";
 ?>
-            <div class="tiles" id="dptiles">
+            <div class="tiles" id="fctiles"><!--
 <?php
 $count = 0;
     
@@ -175,19 +194,19 @@ for ( $period = 0;  $period < 2; $period++ )
             
         if ( $fctime < $now ) continue;
 ?>
-                <div class="tile" onclick="displaydesc('d<?php echo $count; ?>')";>
-                    <div class="fcrain">&#x2119(rain) <strong><?php echo $forecast[ 'Pp' ]; ?>%</strong></div>
+                --><div class="tile" id="t<?php echo $count; ?>" onclick="displaydesc( <?php echo $count; ?> );" >
+                    <div class="fctime"><?php echo $fctime->format( 'D H:i' ); ?></strong></div>
                     <div class="fctemp"><?php echo $forecast[ 'T' ]; ?>&deg;</div>
-                    <div class="fcfeels">Feels like <strong><?php echo $forecast[ 'F' ]; ?>&deg;</strong></div>
-                </div>
+                    <div class="fcrain">&#x2119;&#x1F327; <?php echo $forecast[ 'Pp' ]; ?>% (<?php echo $forecast[ 'F' ]; ?>&deg;)</div>
+                </div><!--
 <?php
-        if ( ++$count == 3 ) break;
+        if ( ++$count == 9 ) break;
     }
         
-    if ( $count == 3 ) break;
+    if ( $count == 9 ) break;
 }
 ?>
-            </div>
+            --></div>
 <?php
 $count = 0;
     
@@ -201,17 +220,17 @@ for ( $period = 0;  $period < 2; $period++ )
             
         if ( $fctime < $now ) continue;
 ?>
-            <div class="description" id="d<?php echo $count; ?>";>
+            <div class="description" id="d<?php echo $count; ?>" >
                 <div><?php echo $fctime->format( 'l jS F Y' ); ?> <strong><?php echo $fctime->format( 'H:i' ); ?></strong></div>
                 <div class="weathertype"><strong><?php echo $weathertype[ $forecast['W'] ]; ?></strong></div>
                 <div>Wind <?php echo $forecast['S']; ?>mph from the <?php echo $forecast['D']; ?></div>
                 <div>Max UV <?php echo $forecast['U']; ?></div>
             </div>
 <?php
-        if ( ++$count == 3 ) break;
+        if ( ++$count == 9 ) break;
     }
         
-    if ( $count == 3 ) break;
+    if ( $count == 9 ) break;
 }
 
 $met = "http://datapoint.metoffice.gov.uk/public/data/txt/wxfcs/regionalforecast/json/$regionid?key=$apikey";
