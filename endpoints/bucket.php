d1d99581e9485cb9b05cca75a8d25f58a9d5db07
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-06-08 22:29:01 +0100

    Update bucket.php
    Slightly tarted up, and handle ordinary GET request.

--- endpoints/bucket.php
+++ endpoints/bucket.php
@@ -1,18 +1,37 @@
 <?php
+//
+// Bucket (bucket.php) - (C) Graham Johnson 2020
+// =============================================
+// Version: 20.06.08.01
+//
+// This is a test application that is being used to learn how to write
+// what SmartThings seem to be calling a 'WebHook Endpoint' automation.
+// It is called 'Bucket' because the SmartThings Developer Workspace
+// doesn't let you rename projects for some reason and it seems daft to
+// have an app name that doesn't match the project name. What it actually
+// does is likely to vary with time.
+//
 
-$body = json_decode( file_get_contents( 'php://input' ), true );
-file_put_contents( 'body.txt', print_r( $body, true ) );
+// Read data from the request body, assuming it is JSON.
+if ( $body = json_decode( file_get_contents( 'php://input' ), true ) )
+{
+    // Save the request body to a file for diagnostic purposes.
+    file_put_contents( 'body.txt', print_r( $body, true ) );
+}
+// Otherwise give the 'lifecycle' key a value to make the code neater.
+else $body = array( 'lifecycle' => 'PLACEHOLDER');
 
+// Check for lifecycle events from SmartThings.
 if ( $body[ 'lifecycle' ]  == 'CONFIRMATION' )
 {
     // https://smartthings.developer.samsung.com/docs/smartapps/lifecycles.html#CONFIRMATION
     
-    // Send required response.
+    // Send the required response.
     $json = array( 'target_url' => 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
     header( 'Content-Type: application/json' );
     echo json_encode( $json );
     
-    // Send GET request to confimation URL.
+    // Send a GET request to the supplied confirmation URL.
     file_get_contents( $body[ 'confirmationData' ][ 'confirmationUrl' ] );
 }
 elseif ( $body[ 'lifecycle' ]  == 'CONFIGURATION' )
@@ -38,6 +57,8 @@ elseif ( $body[ 'lifecycle' ]  == 'CONFIGURATION' )
     }
     elseif ( $body[ 'configurationData' ][ 'phase' ] == 'PAGE' )
     {
+        // Only handling a single page of configuration.
+        
         $json = array( 'configurationData' => 
                             array( 'page' => 
                                         array( 'pageId' => '1',
@@ -64,6 +85,7 @@ elseif ( $body[ 'lifecycle' ]  == 'CONFIGURATION' )
                 
         header( 'Content-Type: application/json' );
         echo json_encode( $json );
+        
         file_put_contents( 'response.txt', json_encode( $json, JSON_PRETTY_PRINT ) );
     }
 }
@@ -74,7 +96,6 @@ elseif ( $body[ 'lifecycle' ]  == 'INSTALL' )
     $json = array( 'installData' => array( 'placeholder' => '' ) );
     header( 'Content-Type: application/json' );
     echo json_encode( $json );
-    file_put_contents( 'response.txt', json_encode( $json, JSON_PRETTY_PRINT ) );
 }
 elseif ( $body[ 'lifecycle' ]  == 'UPDATE' )
 {
@@ -83,15 +104,12 @@ elseif ( $body[ 'lifecycle' ]  == 'UPDATE' )
     $json = array( 'updateData' => array( 'placeholder' => '' ) );
     header( 'Content-Type: application/json' );
     echo json_encode( $json );
-    file_put_contents( 'response.txt', json_encode( $json, JSON_PRETTY_PRINT ) );
     
     // https://smartthings.developer.samsung.com/docs/smartapps/subscriptions.html
     
     $appid = $body[ 'updateData' ][ 'installedApp' ][ 'installedAppId' ];
     $authtoken = $body[ 'updateData' ][ 'authToken' ];
     $url = "https://api.smartthings.com/installedapps/$appid/subscriptions";
-    error_log( $url );
-    error_log( $authtoken );
 
     $ch = curl_init($url);
     
@@ -113,7 +131,6 @@ elseif ( $body[ 'lifecycle' ]  == 'UPDATE' )
 
     $result = curl_exec( $ch );
     curl_close($ch);
-    error_log( $result );
 }
 elseif ( $body[ 'lifecycle' ]  == 'EVENT' )
 {
@@ -131,5 +148,21 @@ elseif ( $body[ 'lifecycle' ]  == 'UNINSTALL' )
     $json = array( 'uninstallData' => array( 'placeholder' => '' ) );
     header( 'Content-Type: application/json' );
     echo json_encode( $json );
-    file_put_contents( 'response.txt', json_encode( $json, JSON_PRETTY_PRINT ) );
 }
+else
+{
+    // This is just a bog standard HTTP GET call.
+?>
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Bucket</title>
+    </head>
+    <body>
+        <h1>Bucket</h1>
+    </body>
+</html>
+<?php
+}
+?>
