f040a66b83cc7869fa674190985869eded5c7a7e
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-06-12 22:24:47 +0100

    Create devices.php
    New file.

---
+++ endpoints/afswl/devices.php
@@ -0,0 +1,38 @@
+<?php
+//
+// Anidea-ST Webhook Library (devices.php) - (C) Graham Johnson 2020
+// =================================================================
+// Version: 20.06.12.00
+//
+
+function afswl_devices_getdescription( $deviceid, $authtoken )
+{
+    $ch = curl_init( "https://api.smartthings.com/v1/devices/$deviceid" );
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $desc = json_decode( curl_exec( $ch ), true );
+    curl_close($ch);
+    
+    afswl_log_asjson( $desc, 'DEVICES_GETDESCRIPTION' );
+    
+    return $desc;
+}
+
+function afswl_devices_getfullstatus( $deviceid, $authtoken )
+{
+    $ch = curl_init( "https://api.smartthings.com/v1/devices/$deviceid/status" );
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $status = json_decode( curl_exec( $ch ), true );
+    curl_close($ch);
+    
+    afswl_log_asjson( $status, 'DEVICES_GETFULLSTATUS' );
+    
+    return $status;
+}
+
+?>
