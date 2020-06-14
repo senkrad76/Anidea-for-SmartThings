01489bf646c32fae7f080d50a8c0173c11b280a7
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-06-14 12:02:52 +0100

    Update afww_capabilities.php

--- automations/orangebucket/anidea-for-webhook-wrapper/afww_capabilities.php
+++ automations/orangebucket/anidea-for-webhook-wrapper/afww_capabilities.php
@@ -1,23 +1,38 @@
 <?php
 //
-// Anidea for WebHook Wrapper (afww_capabilities.php) - (C) Graham Johnson 2020
-// ============================================================================
+// Anidea for WebHook Wrapper (afww_devices.php) - (C) Graham Johnson 2020
+// =======================================================================
 // Version: 20.06.14.00
 //
 
-function afww_capabilities_list( $authtoken )
+function afww_devices_getdescription( $deviceid, $authtoken )
 {
-    $ch = curl_init( 'https://api.smartthings.com/v1/capabilities' );
+    $ch = curl_init( "https://api.smartthings.com/v1/devices/$deviceid" );
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $desc = json_decode( curl_exec( $ch ), true );
+    curl_close($ch);
     
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt( $ch, CURLOPT_POST,           0 );
-    curl_setopt( $ch, CURLOPT_FAILONERROR,    1 );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
-
-    $capabilities = curl_exec( $ch );
-
-    curl_close( $ch );
-
-    return $capabilities;
+    afww_log_asjson( $desc, 'DEVICES_GETDESCRIPTION' );
+    
+    return $desc;
 }
+
+function afww_devices_getfullstatus( $deviceid, $authtoken )
+{
+    $ch = curl_init( "https://api.smartthings.com/v1/devices/$deviceid/status" );
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $status = json_decode( curl_exec( $ch ), true );
+    curl_close($ch);
+    
+    afww_log_asjson( $status, 'DEVICES_GETFULLSTATUS' );
+    
+    return $status;
+}
+
 ?>
