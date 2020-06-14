2f9510e7576e425538b44c7cdd227069f6864539
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-06-14 11:58:21 +0100

    Update afww_capabilities.php

--- automations/orangebucket/anidea-for-webhook-wrapper/afww_capabilities.php
+++ automations/orangebucket/anidea-for-webhook-wrapper/afww_capabilities.php
@@ -1,11 +1,11 @@
 <?php
 //
-// Anidea-ST Webhook Library (afswl_capabilities.php) - (C) Graham Johnson 2020
-// ============================================================================
-// Version: 20.06.13.00
+// Anidea-ST Webhook Wrapper (afww_capabilities.php) - (C) Graham Johnson 2020
+// ===========================================================================
+// Version: 20.06.14.00
 //
 
-function afswl_capabilities_list( $authtoken )
+function afww_capabilities_list( $authtoken )
 {
     $ch = curl_init( 'https://api.smartthings.com/v1/capabilities' );
     
@@ -15,10 +15,7 @@ function afswl_capabilities_list( $authtoken )
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
 
     $capabilities = curl_exec( $ch );
-if( $capabilities === false)
-{
-    error_log( curl_error($ch) );
-}
+
     curl_close( $ch );
 
     return $capabilities;
