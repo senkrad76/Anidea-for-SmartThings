0c574fd88f615468017d4c4c11cfb4fa6aa86681
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-06-18 16:16:03 +0100

    Update afww_subscriptions.php

--- automations/orangebucket/anidea-for-webhook-wrapper/afww_subscriptions.php
+++ automations/orangebucket/anidea-for-webhook-wrapper/afww_subscriptions.php
@@ -1,46 +1,27 @@
 <?php
-//
-// Anidea for WebHook Wrapper (afww_subscriptions.php) - (C) Graham Johnson 2020
-// =============================================================================
-// Version: 20.06.14.00
-//
+/* ---------------------------------------------------------------------------------
+ * (C) Graham Johnson (orangebucket)
+ *
+ * SPDX-License-Identifier: MIT
+ * ---------------------------------------------------------------------------------
+ *
+ * Anidea for WebHook Wrapper (afww_subscriptions.php)
+ * ===================================================
+ * Version: 20.06.18.00
+ */
 
 function afww_subscriptions_create( $appid, $authtoken, $sub )
 {
-    $ch = curl_init( "https://api.smartthings.com/v1/installedapps/$appid/subscriptions" );
-
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $sub ) );
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $result = curl_exec( $ch );
-    curl_close($ch);
-    
-    afww_log_asjson( $sub, 'SUBSCRIPTION_REQUEST' );
+    return afww_curl_api_post( "/installedapps/$appid/subscriptions", $authtoken, $sub );
 }
 
 function afww_subscriptions_deleteall( $appid, $authtoken )
 {
-    $ch = curl_init( "https://api.smartthings.com/installedapps/$appid/subscriptions" );
-    
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $result = curl_exec( $ch );
-    curl_close($ch);
+    return afww_curl_api_delete( "/installedapps/$appid/subscriptions", $authtoken );
 }
 
 function afww_subscriptions_list( $appid, $authtoken )
 {       
-    $ch = curl_init( "https://api.smartthings.com/installedapps/$appid/subscriptions" );
-    
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $subs = curl_exec( $ch );
-    curl_close($ch);
-    
-    afww_log_asjson( json_decode( $subs, true), 'SUBSCRIPTION_LIST' );
+    return afww_curl_api_get( "/installedapps/$appid/subscriptions", $authtoken );
 }
 ?>
