e58113aeac39c5ee135717680b5d8e6b2fc6bad8
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-06-18 16:09:32 +0100

    Update afww_capabilities.php

--- automations/orangebucket/anidea-for-webhook-wrapper/afww_capabilities.php
+++ automations/orangebucket/anidea-for-webhook-wrapper/afww_capabilities.php
@@ -7,38 +7,16 @@
  *
  * Anidea for WebHook Wrapper (afww_capabilities.php)
  * ==================================================
- * Version: 20.06.15.00
+ * Version: 20.06.18.00
  */
 
 function afww_capabilities_list( $authtoken )
 {
-    $ch = curl_init( 'https://api.smartthings.com/v1/capabilities' );
-    
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt( $ch, CURLOPT_POST,           0 );
-    curl_setopt( $ch, CURLOPT_FAILONERROR,    1 );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
-
-    $capabilities = curl_exec( $ch );
-
-    curl_close( $ch );
-
-    return $capabilities;
+    return afww_curl_api_get( '/capabilities', $authtoken );
 }
 
 function afww_capabilities_get( $capid, $capversion, $authtoken )
 {
-    $ch = curl_init( "https://api.smartthings.com/v1/capabilities/$capid/$capversion" );
-    
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt( $ch, CURLOPT_POST,           0 );
-    curl_setopt( $ch, CURLOPT_FAILONERROR,    1 );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array( "Authorization: Bearer $authtoken" ) );
-
-    $capability =  curl_exec( $ch );
-
-    curl_close( $ch );
-    
-    return $capability;
+    return afww_curl_api_get( "/capabilities/$capid/$capversion", $authtoken );
 }
 ?>
