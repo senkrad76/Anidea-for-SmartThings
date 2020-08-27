e7cf77fd0ac1a0d18417623737890e121767f9c7
orangebucket <39342399+orangebucket@users.noreply.github.com>
2020-08-27 19:25:01 +0100

    Update afww_devices.php

--- automations/orangebucket/anidea-for-webhook-wrapper/afww_devices.php
+++ automations/orangebucket/anidea-for-webhook-wrapper/afww_devices.php
@@ -7,7 +7,7 @@
  *
  * Anidea for WebHook Wrapper (afww_devices.php)
  * =============================================
- * Version: 20.06.18.00
+ * Version: 20.08.27.00
  */
 
 function afww_devices_getdescription( $deviceid, $authtoken )
@@ -17,7 +17,7 @@ function afww_devices_getdescription( $deviceid, $authtoken )
 
 function afww_devices_getfullstatus( $deviceid, $authtoken )
 {
-    return afww_curl_api_get( "/devices/$deviceid/status" );
+    return afww_curl_api_get( "/devices/$deviceid/status", $authtoken );
 }
 
 ?>
